f702931f5f94b7ba49e69dff61951feeaab48c13
LeeLuiza <Laurabeck1315@gmail.com>
2024-02-07 19:49:33 +0500

    commit-1

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,16 +1,25 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
     String text;
     Integer likes;
+    private Date creationDate;
 
     public Post(String text){
         this.text = text;
     }
 
-    public Post(String text, Integer likes){
+    public Post(String text, Date creationDate){
+        this.text = text;
+        this.creationDate = creationDate;
+    }
+
+    public Post(String text, Integer likes, Date creationDate){
         this.text = text;
         this.likes = likes;
+        this.creationDate = creationDate;
     }
 
     public String getText() {
@@ -19,5 +28,8 @@ public class Post {
 
     public Integer getLikes() {
         return likes;
+    }
+    public Date getCreationDate() {
+        return creationDate;
     }
 }
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,12 +3,14 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class PostService {
     public Post[] listAllPosts(){
-        Post firstPost = new Post("Пост 1", 12);
-        Post secondPost = new Post("Пост 2", 23);
-        Post thirdPost = new Post("Пост 3",52);
+        Post firstPost = new Post("Пост 1", 12, new Date());
+        Post secondPost = new Post("Пост 2", 23, new Date());
+        Post thirdPost = new Post("Пост 3",52, new Date());
         Post[] arr = new Post[] {firstPost, secondPost, thirdPost};
 
         return arr;
